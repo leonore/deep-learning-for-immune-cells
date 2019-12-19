0cf8367c7fe66d906bdaa624d15fb58bfb5e10ca
Leonore Papaloizos <2264897V@student.gla.ac.uk>
2019-12-19 11:32:14 +0000

    cleaning up redundant code and files

--- src/dataset_helpers.py
+++ src/dataset_helpers.py
@@ -7,16 +7,6 @@ from skimage.transform import rescale, resize, downscale_local_mean
 from skimage import filters
 from PIL import Image
 
-# IMAGE FORMATTING OPERATIONS
-
-imw, imh, c = 192, 192, 1
-
-def reshape(img, w=imw, h=imh, c=c):
-    if c > 1:
-      return np.reshape(img, (w, h, c))
-    else:
-      return np.reshape(img, (w, h))
-
 def reformat(img, type):
     formatted = (img).astype(type)
     return Image.fromarray(formatted)
@@ -25,11 +15,6 @@ def center_crop(img, size=imw):
     to_crop = (img.shape[0]-size)/2
     image_resized = skimage.util.crop(img, (to_crop, to_crop))
     return image_resized
-
-def normalise(img):
-    # normalise 16-bit TIF image
-    img = np.full_like(img, img)
-    return img / 65535.0
 
 def mean_clip(x):
     mean = np.mean(x)
@@ -45,24 +30,15 @@ def max_normalise(x):
     max = np.max(x)
     return x / max
 
-# change this function to another pre-processing function
-# if needed
-def preprocess(x):
-    return max_normalise(low_clip(x))
-
-
 ## DATASET OPERATIONS
 
-def even_round(num):
-    return round(num/2.)*2
-
-# need to find a way to shuffle this pairwise!
-def dataset_split(dataset, labels, test_size=0.2):
-    test_size = even_round(len(dataset)*test_size)
-    x_train, x_test = dataset[:-test_size], dataset[-test_size:]
-    y_train, y_test = labels[:-test_size], labels[-test_size:]
-    return x_train, x_test, y_train, y_test
-
+# train_test_split simplified
+def unishuffle(a, b, random_state=RS):
+    assert len(a) == len(b)
+    if random_state:
+        np.random.seed(RS)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
 
 def is_dmso(file):
     # file format: folder/CKX - L - 00(...)
@@ -88,89 +64,6 @@ def is_dmso(file):
     else:
         print("No CK found")
     return False
-
-
-def filenames_to_labels(filenames, folder="/Users/Leonore/Documents/Workspace/l4proj/data/processed/"):
-    # 0: unstimulated
-    # 1: OVA
-    # 2: ConA
-    # 3: empty
-    labels = []
-    DMSO = []
-
-    for file in filenames:
-        labels.append(get_label(file))
-        if is_dmso(file):
-            DMSO.append(len(labels))
-    return labels, DMSO
-
-
-def read_folder_filenames(folder):
-    return [os.path.join(folder, f)for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f)) and f[0] != '.' and "Brightfield" not in f]
-
-
-def resize_images(folder='/Users/Leonore/Documents/Workspace/l4proj/data/raw/',
-                  dst="/Users/Leonore/Documents/Workspace/l4proj/data/processed/CK22/", w=200, h=200):
-    filenames = [os.path.join(folder, f) for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
-    for file in filenames:
-        if not os.path.exists(os.path.join(dst, file.replace(folder, ''))):
-            try:
-                image = imread(file)
-                image_resized = center_crop(image, w)
-                img = reformat(image_resized)
-                img.save(dst + file.replace(folder, ''))
-            except Exception as e:
-                print(e)
-                print("{} is causing issues".format(file))
-
-
-def images_to_dataset(folder="/Users/Leonore/Documents/Workspace/l4proj/data/processed/", w=192, h=192, process=True):
-    filenames = []
-    for ck in ["CK19", "CK21", "CK22"]:
-        filenames.extend(read_folder_filenames(folder+ck))
-    filenames = sorted(filenames)
-    dataset = np.ndarray(shape=(len(filenames), w, h), dtype=np.float32)
-    i = 0
-    for file in filenames:
-        image = imread(file)
-        image_resized = center_crop(image, size=w)
-        if process:
-            image_resized = preprocess(image_resized)
-        dataset[i] = image_resized
-        i += 1
-    print("All files formatted into dataset.")
-    return dataset, filenames
-
-
-def sliding_window(img, dest_size, rgb=False):
-    new_img = np.full_like(img, img)
-
-    size = img.shape[0]
-    if dest_size > size or dest_size % 2 != 0:
-        raise Exception("destination size is bigger than picture size or destination size is not even")
-
-    qty = size//dest_size
-    if size % dest_size != 0:
-        # need to crop out the left and bottom (less significant in dataset)
-        crop = size-dest_size*qty
-        new_img = new_img[crop:, :-crop]
-
-    if rgb:
-        windows = np.ndarray(shape=(qty**2, dest_size, dest_size, 3), dtype=np.float32)
-    else:
-        windows = np.ndarray(shape=(qty**2, dest_size, dest_size), dtype=np.float32)
-
-    i = 0
-    for row in range(qty):
-        y = row*dest_size
-        x = 0
-        for col in range(qty):
-            #print("x:coord {},{} - y:coord {},{}".format(x, x+dest_size, y, y+dest_size))
-            windows[i] = new_img[x:x+dest_size, y:y+dest_size]
-            x += dest_size
-            i += 1
-
-    return windows
 
 def get_label(filename):
     # 0: unstimulated
@@ -213,3 +106,37 @@ def get_label(filename):
         return False
 
     return label
+
+def read_folder_filenames(folder):
+    return [os.path.join(folder, f)for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f)) and f[0] != '.' and "Brightfield" not in f]
+
+
+def sliding_window(img, dest_size, rgb=False):
+    new_img = np.full_like(img, img)
+
+    size = img.shape[0]
+    if dest_size > size or dest_size % 2 != 0:
+        raise Exception("destination size is bigger than picture size or destination size is not even")
+
+    qty = size//dest_size
+    if size % dest_size != 0:
+        # need to crop out the left and bottom (less significant in dataset)
+        crop = size-dest_size*qty
+        new_img = new_img[crop:, :-crop]
+
+    if rgb:
+        windows = np.ndarray(shape=(qty**2, dest_size, dest_size, 3), dtype=np.float32)
+    else:
+        windows = np.ndarray(shape=(qty**2, dest_size, dest_size), dtype=np.float32)
+
+    i = 0
+    for row in range(qty):
+        y = row*dest_size
+        x = 0
+        for col in range(qty):
+            #print("x:coord {},{} - y:coord {},{}".format(x, x+dest_size, y, y+dest_size))
+            windows[i] = new_img[x:x+dest_size, y:y+dest_size]
+            x += dest_size
+            i += 1
+
+    return windows
--- src/readme.md
+++ src/readme.md
@@ -1,15 +1,12 @@
 # Readme
 
 ### Jupyter notebooks
-* `cell_autoencoder.ipynb` includes the autoencoder model and clustering algorithm being tuned
+* `cell_autoencoder.ipynb` collates all the research done with autoencoders
 * `baseline_performance.ipynb` reports on the clustering algorithm's base performance with unmodified datasets
-* `cell_histograms.ipynb` contains research on the structure of the dataset for better pre-processing
 * `compress_files.ipynb` is the code used to compress images into efficient NPZ files for re-use locally and on Google Colab
-* `visualise_tsne.ipynb` is the code used to visualise step-by-step t-sne clustering, returning a GIF
 * `clustering_mnist.ipynb` contains the tutorial code for clustering MNIST with PCA, PCA+t-sne, and UMAP
 
 ### Helper files
-* `prepare_dataset.py` contains the code used to transfer the needed images from the OneDrive dataset for transformation
 * `dataset_helpers.py` contains helper functions to prepare the dataset
 * `plot_helpers.py` contains helper functions to plot different results
 
