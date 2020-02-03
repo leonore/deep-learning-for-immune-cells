cb5dee64069d4319bffd0d84b60a7fc3a4f33e22
Leonore Papaloizos <2264897V@student.gla.ac.uk>
2020-02-03 16:57:22 +0000

    move ipynb files, make_autoencoder fn

--- src/cell_autoencoder.py
+++ src/cell_autoencoder.py
@@ -1,21 +1,44 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
-import os, copy
-
-from sklearn.manifold import TSNE
-import umap
 
 from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, Flatten, Reshape, PReLU
 from keras.models import Model
-
-from dataset_helpers import minmax, is_faulty
-from dataset_helpers import get_label
-from plot_helpers import show_image, plot_range
-from segmentation import threshold, get_mask
-
 
 imw = 192
 imh = 192
 c = 3
 RS = 2211
+
+def make_autoencoder():
+    input_img = Input(shape=(imw, imh, c))
+
+    x = Conv2D(64, (3, 3), padding='same')(input_img)
+    x = PReLU()(x)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(32, (3, 3), padding='same')(x)
+    x = PReLU()(x)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(32, (3, 3), padding='same')(x)
+    x = PReLU()(x)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(16, (3, 3), padding='same', strides=2)(x)
+    x = PReLU()(x)
+
+    encoded = Flatten()(x)
+
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2D(32, (3, 3), padding='same')(x)
+    x = PReLU()(x)
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2D(32, (3, 3), padding='same')(x)
+    x = PReLU()(x)
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2D(64, (3, 3), padding="same")(x)
+    x = PReLU()(x)
+    x = UpSampling2D((2, 2))(x)
+    decoded = Conv2D(c, (3, 3), activation='sigmoid', padding='same')(x)
+
+    decoder = Model(input_img, decoded)
+    encoder = Model(input_img, encoded)
+    decoder.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return decoder, encoder
