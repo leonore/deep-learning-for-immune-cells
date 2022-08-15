e23a83af840b3a17a477bc8268353f299ee0894d
leonore <leonore@protonmail.com>
2022-08-15 11:44:18 -0400

    functions for compressing images into npz file

--- src/dataset_helpers.py
+++ src/dataset_helpers.py
@@ -38,6 +38,9 @@ def clip(x):
 def minmax(x):
     return (x-np.min(x))/(np.max(x)-np.min(x))
 
+def preprocess(x):
+    return minmax(clip(x))
+
 ## DATASET OPERATIONS
 
 def even_round(num):
@@ -49,6 +52,31 @@ def dataset_split(dataset, labels, test_size=0.2):
     x_train, x_test = dataset[:-test_size], dataset[-test_size:]
     y_train, y_test = labels[:-test_size], labels[-test_size:]
     return x_train, x_test, y_train, y_test
+
+def is_dmso(file):
+    # file format: folder/CKX - L - 00(...)
+
+    # only file, no folder
+    file = file.split('/')[-1].split("(")[0]
+
+    # get letter for DMSO indices
+    letter = file.split('-')[1].strip()
+
+    # number
+    label = file[-2:].strip()
+
+    # cell case
+    ck = file[:4]
+
+    if ck == "CK19":
+        if label in ["5", "8", "11", "15", "18", "21"] and letter in ["N", "O", "P"]:
+            return True
+    elif ck == "CK21" or ck == "CK22":
+        if label != 3 and letter in ["H", "P"]:
+            return True
+    else:
+        print("No CK found")
+    return False
 
 def filenames_to_labels(filenames, folder="/Users/Leonore/Documents/Workspace/l4proj/data/processed/"):
     # 0: unstimulated
@@ -89,8 +117,10 @@ def filenames_to_labels(filenames, folder="/Users/Leonore/Documents/Workspace/l4
         labels.append(label)
     return labels, DMSO
 
+
 def read_folder_filenames(folder):
     return [os.path.join(folder, f)for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f)) and f[0] != '.' and "Brightfield" not in f]
+
 
 def resize_images(folder='/Users/Leonore/Documents/Workspace/l4proj/data/raw/',
                   dst="/Users/Leonore/Documents/Workspace/l4proj/data/processed/CK22/", w=200, h=200):
@@ -121,3 +151,77 @@ def images_to_dataset(folder="/Users/Leonore/Documents/Workspace/l4proj/data/pro
         i += 1
     print("All files formatted into dataset.")
     return dataset, filenames
+
+
+def sliding_window(img, dest_size):
+    new_img = np.full_like(img, img)
+
+    size = img.shape[0]
+    if dest_size > size or dest_size % 2 != 0:
+        raise Exception("destination size is bigger than picture size or destination size is not even")
+
+    qty = size//dest_size
+    if size % dest_size != 0:
+        # need to crop out the left and bottom (less significant in dataset)
+        crop = size-dest_size*qty
+        new_img = new_img[crop:, :-crop]
+
+    windows = np.ndarray(shape=(qty**2, dest_size, dest_size), dtype=np.float32)
+
+    i = 0
+    for row in range(qty):
+        y = row*dest_size
+        x = 0
+        for col in range(qty):
+            #print("x:coord {},{} - y:coord {},{}".format(x, x+dest_size, y, y+dest_size))
+            windows[i] = new_img[x:x+dest_size, y:y+dest_size]
+            x += dest_size
+            i += 1
+
+    return windows
+
+def get_label(filename):
+    # 0: unstimulated
+    # 1: OVA
+    # 2: ConA
+    # 3: empty
+
+    # filename format: folder/CKX - L - 00(...)
+    file = file.split("/")[-1].split("(")[0]
+
+    # get letter for DMSO indices
+    letter = file.split('-')[1].strip()
+
+    # get number
+    number = file[-2:].strip()
+
+    # get plate layout number
+    ck = file[:4]
+
+    # DMSO = []
+
+    if ck == "CK19":
+        #if number in ["5", "8", "11", "15", "18", "21"] and letter in ["N", "O", "P"]:
+        #   DMSO.append(len(labels))
+        if number in ["3", "4", "5", "6", "7", "8", "24"]:
+            label = 0
+        elif number in ["9", "10", "11", "13", "14", "15", "23"]:
+            label = 1
+        elif number in ["16", "17", "18", "19", "20", "21", "22"]:
+            label = 2
+        else:
+            label = 3
+    elif ck == "CK21" or ck == "CK22":
+        if number in ["02", "03", "04", "05", "06", "07", "08", "09", "10", "11"]:
+            label = 0
+        elif int(number) in range(14, 24):
+            label = 2
+        else:
+            label = 3
+        #if label != 3 and letter in ["H", "P"]:
+        #    DMSO.append(len(labels))
+    else:
+        print("No CK found")
+        return False
+
+    return label
