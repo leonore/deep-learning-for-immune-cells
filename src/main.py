b0a5e99e3ff148be85486f4b1340328fee045306
leonore <leonore@protonmail.com>
2022-08-15 11:44:26 -0400

    combined autoencoder and regression into one models file

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ parser = argparse.ArgumentParser(description='Evaluate a model by training with 
 
 parser.add_argument('--input', '-i', action='store', type=str, help='Compressed NPZ file to process images from', required=True)
 parser.add_argument('--metrics', '-m', action='store', type=str, help='Compressed NPZ file containing metrics captured from the images', required=True)
-parser.add_argument('--weights', '-w', action='store', nargs=3, type=str, help='Three h5 files for decoder, encoder, regression trained models (decoder.h5, encoder.h5)', required=False)
+parser.add_argument('--weights', '-w', action='store', nargs=3, type=str, help='Three h5 files for decoder, encoder, regression trained models', required=False)
 parser.add_argument('--mask', action='store_false', help="Mask out the background of images using K-means")
 parser.add_argument('--live', '-l', action='store_true', help='Make clustering visualisation live')
 parser.add_argument('--tag', action='store', help="Tag for saving figures. If not specified the files won't be saved.")
@@ -25,13 +25,11 @@ mask = args.mask
 
 import numpy as np
 import tensorflow.keras
-from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.models import load_model
 
-import clustering.run_both
-import cell_autoencoder.evaluate
-import regression_model.evaluate
-from cell_autoencoder import make_autoencoder
-from regression_model import make_regression
+from models import make_autoencoder, make_regression, train
+from models import evaluate_regression, evaluate_autoencoder
+from clustering import run_both
 
 from evaluation_helpers import plot_clusters, plot_live
 from dataset_helpers import get_label, combine_images, efficient_shuffle
@@ -45,14 +43,17 @@ x = npzfile['x'] # images
 
 # preprocess
 print("Combining images.")
-x_combined = preprocess(x, mask=mask)
+x_combined = combine_images(x, mask=mask)
 
 print("Metrics are being loaded from {}".format(metrics))
 metrics = np.load(metrics)
 y_combined = metrics['y_combined']
 y_overlaps = metrics['y_overlaps']
 
-
+efficient_shuffle(x_combined, y_combined, y_overlaps)
+x_combined = x_combined[-10000:]
+y_combined = y_combined[-10000:]
+y_overlaps = y_overlaps[-10000:]
 
 # make autoencoder
 if weights:
@@ -61,6 +62,7 @@ if weights:
     print("Encoder file is {}".format(weights[1]))
     encoder = load_model(weights[1])
     print("Regression file is {}".format(weights[2]))
+    regression = load_model(weights[2])
 else:
     # not recommended on local - could take hours
     print("Training autoencoder... be aware this might take a long time")
@@ -68,15 +70,16 @@ else:
     train(decoder, x_combined)
     print("Training regression model... be aware this might take a long time")
     regression = make_regression(encoder)
+    train(encoder, x_combined)
 
 # evaluate
-cell_autoencoder.evaluate(decoder, x_combined, x_combined, tag)
+evaluate_autoencoder(decoder, x_combined, tag)
 
 # run clustering
 encoded_imgs = encoder.predict(x_combined)
 
 print("Clustering encoded images")
-x_tsne, x_umap = clustering.run_both(encoded_imgs, random_state=RS)
+x_tsne, x_umap = run_both(encoded_imgs, random_state=RS)
 
 if args.live:
     print("Visualisation being plotted live...")
@@ -90,4 +93,4 @@ else:
 print("Running regression using encoder model...")
 
 y_pred = regression.predict(x_combined)
-regression_model.evaluate(y_pred, y_true, tag)
+evaluate_regression(y_overlaps, y_pred, y_combined, tag)
