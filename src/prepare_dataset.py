6a28932f68a3386713134312abfe01e6fb254aa8
leonore <leonore@protonmail.com>
2022-08-15 11:44:16 -0400

    made dataset bigger; prepare move to colab

--- src/prepare_dataset.py
+++ src/prepare_dataset.py
@@ -2,11 +2,11 @@
 # TO LOCAL DATA FOR LOCAL USE
 
 import os
-from shutil import copyfile, rmtree
+from shutil import copyfile
 
 def copy_to(dataset_folder):
     ignored = 0
-    data_folder = "/Users/Leonore/University of Glasgow/Hannah Scales - Image analysis data/CK19/TCell_DCellInteraction10X_FinV_2014.07.04.12.14.45"
+    data_folder = "/Users/Leonore/Downloads/OneDrive_2_07-11-2019"
     files = [f for f in os.listdir(data_folder)]
     for filename in files:
         src = os.path.join(data_folder, filename)
@@ -15,35 +15,7 @@ def copy_to(dataset_folder):
             copyfile(src, dst)
         else:
             ignored += 1
+            print(filename + " ignored")
     print("{} were ignored, not appropriate data".format(ignored))
 
-def clean(dataset_folder):
-    rmtree(dataset_folder)
-
-#clean('/Users/Leonore/Documents/Workspace/l4proj/data/raw/')
 copy_to('/Users/Leonore/Documents/Workspace/l4proj/data/raw/')
-
-
-# import shutil
-# from PIL import Image
-#
-# def convert(dataset_path):
-#     for _, _, files in os.walk(dataset_path):
-#         for filename in files:
-#             filepath = os.path.join(dataset_path, filename)
-#             image = Image.open(filepath)
-#             image = image.convert(mode="RGB")
-#             image.save(filepath + 'my.jpeg')
-#
-# def resize(dataset_path, new_width, new_height):
-#     for _, _, files in os.walk(dataset_path):
-#         for filename in files:
-#             filepath = os.path.join(dataset_path, filename)
-#             img = Image.open(filepath)
-#             img = img.convert('RGB')
-#             img.thumbnail((int(new_width), int(new_height)))
-#             filepath = filepath.rstrip('.tif')
-#             img.save(filepath + '.jpeg', 'JPEG')
-#
-# duplicate_directory('../data/raw', '../data/processed')
-# convert('../data/processed')
